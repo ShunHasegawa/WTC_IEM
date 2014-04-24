795e5126e25ac5293b33d2887b212cefcd334b1f
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-24 23:11:27 +1000

    summary table is prodeuced

--- R/Analysis.R
+++ R/Analysis.R
@@ -16,40 +16,57 @@ library(ggplot2)
 
 source("R/functions/functions.R")
 
-res <- read.csv("Data/WTC_IEM.csv", colClasses = c("Chamber" = "factor", "Location" = "factor", "insertion" = "character", 
+iem <- read.csv("Data/WTC_IEM.csv", colClasses = c("Chamber" = "numeric", "Location" = "factor", "insertion" = "character", 
                                                    "sampling" = "character", "Time" = "factor"))
-res$insertion <- dmy(res$insertion)
-res$sampling <- dmy(res$sampling)
-res <- res[complete.cases(res), ]
-res <- droplevels(res)
+
+iem$Chamber <- factor(ifelse(iem$Chamber < 10, paste("0", iem$Chamber, sep = ""), iem$Chamber)) #"01" is easier to organise than "1"
+iem$insertion <- as.Date(dmy(iem$insertion))
+iem$sampling <- as.Date(dmy(iem$sampling))
+iem <- iem[complete.cases(iem), ]
+iem <- droplevels(iem)
+iem$date <- as.Date(ave(apply(cbind(iem$insertion, iem$sampling), 1, mean), iem$Time), origin = origin) # same date for same date
+
+#################
+# Summary table #
+#################
+iem.mlt <- melt(iem, id = c("Time", "insertion", "sampling", "Chamber", "Location", "temp", "date"))
+
+# chamber mean
+ChSmmryTbl <- dlply(iem.mlt, .(variable), function(x) CreateTable(x, fac = "Chamber"))
+
+# treat mean
+ChMean <- ddply(iem.mlt, .(Time, date, temp, Chamber, variable), summarise, value = mean(value, na.rm = TRUE)) 
+TrtSmmryTbl <- dlply(ChMean, .(variable), function(x) CreateTable(x, fac = "temp"))
+
+# export as excel file
 
 #############
 # Phosphate #
 #############
 
 ## Stats ##
-names(res)
-summary(subset(res, po < max(po)))
-boxplot(log(po+0.0001) ~ temp:Time, data = subset(res, po < max(po)))
-boxplot(log(po+0.0001) ~ temp:Time, data = res)
+names(iem)
+summary(subset(iem, po < max(po)))
+boxplot(log(po+0.0001) ~ temp:Time, data = subset(iem, po < max(po)))
+boxplot(log(po+0.0001) ~ temp:Time, data = iem)
 
-boxplot(sqrt(no) ~ temp:Time, data = subset(res, no < max(no)))
-boxplot(no ~ temp:Time, data = res)
-boxplot(nh ~ temp:Time, data = subset(res, nh < max(nh)))
+boxplot(sqrt(no) ~ temp:Time, data = subset(iem, no < max(no)))
+boxplot(no ~ temp:Time, data = iem)
+boxplot(nh ~ temp:Time, data = subset(iem, nh < max(nh)))
 
-res$id <- res$Chamber:res$Location
+iem$id <- iem$Chamber:iem$Location
 
-m1 <- lme(log(po+0.0001) ~ temp * Time, random = ~ 1|Chamber/Location, data = res, method = "ML", subset = po < max(po))
-m1 <- lme(log(po+0.0001) ~ temp * Time, random = ~ 1|Chamber/Location, data = res, method = "ML")
-m1 <- lme((po+0.0001)^(1/3) ~ temp * Time, random = ~ 1|Chamber/Location, data = res, method = "ML", subset = po < max(po))
+m1 <- lme(log(po+0.0001) ~ temp * Time, random = ~ 1|Chamber/Location, data = iem, method = "ML", subset = po < max(po))
+m1 <- lme(log(po+0.0001) ~ temp * Time, random = ~ 1|Chamber/Location, data = iem, method = "ML")
+m1 <- lme((po+0.0001)^(1/3) ~ temp * Time, random = ~ 1|Chamber/Location, data = iem, method = "ML", subset = po < max(po))
 Anova(m1)
 
 qqnorm(residuals(m1))
 qqline(residuals(m1))
 
 
-m2 <- lme(log(po+0.0001) ~ temp * Time, random = ~ 1|id, data = res, method = "ML")
-m3 <- lme(log(po+0.0001) ~ temp * Time, random = ~ 1|Chamber/id, data = res, method = "ML")
+m2 <- lme(log(po+0.0001) ~ temp * Time, random = ~ 1|id, data = iem, method = "ML")
+m3 <- lme(log(po+0.0001) ~ temp * Time, random = ~ 1|Chamber/id, data = iem, method = "ML")
 anova(m1, m2, m3)
 summary(m2)
 
