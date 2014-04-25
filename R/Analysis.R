ce1f6930084bba28b2511d4acae58d9591b17088
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-25 13:07:48 +1000

    model comparison

--- R/Analysis.R
+++ R/Analysis.R
@@ -43,24 +43,30 @@ bxplts(value= "po", ofst= .0001, data= iem)
 # log transformation seems best
 
 # autocorelation
-m1 <- lme(log(po + .0001) ~ temp * Time, random = ~1|Chamber/Location, data = iem)
+m1 <- lme(log(po + .0001) ~ temp * Time, random = ~1|Chamber/Location, data = iem, method = "ML")
 atcr.cmpr(m1)
 # no need for auto-correlation
 
 # random factor structure
 iem$id <- iem$Chamber:iem$Location
-m2 <- lme(log(po + .0001) ~ temp * Time, random = ~1|id, data = iem)
-m3 <- lme(log(po + .0001) ~ temp * Time, random = ~1|Chamber, data = iem)
-anova(m1, m2, m3)
+m2 <- lme(log(po + .0001) ~ temp * Time, random = ~1|id, data = iem, method = "ML")
+m3 <- lme(log(po + .0001) ~ temp * Time, random = ~1|Chamber, data = iem, method = "ML")
+m4 <- lme(log(po + .0001) ~ temp * date, random = ~1|id, data = iem, method = "ML")
+
+
+anova(m1, m2, m3, m4)
 summary(m3)
 
 # m3 is slightly better, between-chamber variation is less important within-chanmber (=between location within chamber)
-MdlSmpl(m2)
-m3 <- update(m2, ~ . -temp:Time)
 MdlSmpl(m3)
-Fml <- MdlSmpl(m3)$model.reml
+m4 <- update(m3, ~ . -temp:Time)
+MdlSmpl(m4)
+Fml <- MdlSmpl(m4)$model.reml
 Anova(Fml)
 plot(allEffects(Fml))
+qqnorm(Fml, ~ranef(.))
+
+qqline(Fml)
 
 ########
 # Figs #
