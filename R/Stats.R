f89fd58998567a9ac7c8fb44f79f484b8ac0f726
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-14 10:04:24 +1000

    merge soil moisture and temp data

--- R/Stats.R
+++ R/Stats.R
@@ -1,3 +1,35 @@
+#####################################
+# merge soil moisture and temp data #
+#####################################
+load("Data/WTC_soilMoistTemp_Chamber_DailySummary.RData")
+
+# restructure
+names(soilChmSmry)[4] <- "probe"
+SoilChMlt <- melt(soilChmSmry, id = c("Date", "Chamber", "temp", "probe"))
+SoilCh <- cast(SoilChMlt, Date + Chamber + temp ~ probe + variable)
+
+
+# chamber mean for IEM
+IEM_ChMean <- ddply(iem, .(Time, insertion, sampling, date, Chamber, temp), 
+                    function(x) colMeans(x[,c("no", "nh", "po")], na.rm = TRUE))
+names(IEM_ChMean)[4] <- "Date"
+
+
+# mean of soil vars during incubation period
+head(SoilCh)
+
+data = SoilCh
+SoilIncSampMean <- function(insertion, sampling, Chm, data = SoilCh){
+  a <- subset(data, Date >= insertion & Date >= sampling & Chamber == Chm)
+  vars <- names(a)[which(!names(a) %in% c("Date", "Chamber", "temp"))]
+  b <- ddply(a, .(Chamber), function(x) colMeans(x[, vars], na.rm = TRUE))
+  return(cbind(insertion, sampling, b))
+}
+
+
+IEM_DF <- ddply(IEM_ChMean, .(Time, Date, insertion, sampling, Chamber, temp, no, nh, po),
+                function(x) SoilIncSampMean(insertion= x$insertion, sampling= x$sampling, Chm = x$Chamber))
+
 ###########
 # Nitrate #
 ###########
