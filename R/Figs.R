5fbb82c0935ab3aaa0e11074b5ab71945483f9e3
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-07-10 22:50:52 +0100

    figure for thesis

--- R/Figs.R
+++ R/Figs.R
@@ -16,6 +16,52 @@ TrtFg <- dlply(TrtMean, .(variable), PltTmpMean)
 fls <- paste("Output/Figs/WTC_IEM_Temp_", c("Nitrate", "Ammonium", "phosphate"),sep = "")
 l_ply(1:3, function(x) ggsavePP(filename = fls[x], plot = TrtFg[[x]], width = 6, height = 3))
 
+########################
+# Plot for publication #
+########################
+# theme
+theme_set(theme_bw())
+
+# ymax value for each variable
+TrtMean$Date <- TrtMean$date
+TrtMean$Time <- TrtMean$time
+ymaxDF <- ddply(TrtMean, .(variable), function(x) max(x$Mean + x$SE, na.rm = TRUE))
+
+# load contrastDF to annotate stat result and combine with max values from
+# TrtMean as y position
+load("output//data/WTC_IEM_ContrastDF.RData")
+
+Antt_CntrstDF <- merge(ContrastDF, 
+                       ddply(TrtMean, .(Date, variable), summarise, yval = max(Mean + SE)),
+                       # this return maximum values
+                       by = c("Date", "variable"), all.x = TRUE)
+
+
+Antt_CntrstDF$temp <- "amb" # co2 column is required as it's used for mapping
+
+# Stat result
+load("output//data//CO2Time_Stat.RData")
+
+# ylables for facet_grid
+ylabs <- list(
+  'no' = expression(NO[3]^"-"),
+  'nh' = expression(NH[4]^"+"),
+  'po' = expression(PO[4]^"3-"))
+
+
+ylab_label <- function(variable, value){
+  return(ylabs[value])
+}
+
+p <- WBFig(data = TrtMean, ylab = expression(IEM*-adsorbed~nutrients~(ng~cm^"-2"~d^"-1")),
+           StatRes = Stat_TempTime, 
+           StatY = ymaxDF[ , 2]*1.15) +
+  geom_text(data = Antt_CntrstDF, aes(x = Date, y = yval, label = stars), vjust = 0)
+p
+ggsavePP(filename = "output//figs/Manuscript/WTC_IEM", plot = p, width = 6.65, height = 6.65)
+
+
+
 ############################
 # Fig for BES presentaiton #
 ############################
